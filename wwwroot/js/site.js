6163216dbff5e8360d9555e2124bd0d853d1b603
jorgellitra <jorge.llitra@gmail.com>
2025-05-17 09:30:27 +0100

    aaa

--- wwwroot/js/site.js
+++
@@ -1,8 +0,0 @@
-
-window.playAllVideos = () => {
-    const videos = document.querySelectorAll('.auto-play-video');
-    videos.forEach(video => {
-        video.muted = true;
-        video.play().catch(e => console.warn("Autoplay prevented:", e));
-    });
-};
